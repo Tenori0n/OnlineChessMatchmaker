7af81651ef8d6fce77305d6b317870049efff0ee
Tenori0n <maeestrod@gmail.com>
2025-10-10 10:09:36 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,23 @@ Route::get('/match', [\App\Http\Controllers\ChessMatchControllerApi::class, 'ind
 
 Route::get('/match/{id}', [\App\Http\Controllers\ChessMatchControllerApi::class, 'show']);
 
-Route::get('/turn', [\App\Http\Controllers\TurnControllerApi::class, 'index']);
-
 Route::get('/turn/{id}', [\App\Http\Controllers\TurnControllerApi::class, 'show']);
 
 Route::get('/user', [\App\Http\Controllers\UserControllerApi::class, 'index']);
 
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
 Route::get('/user/{id}', [\App\Http\Controllers\UserControllerApi::class, 'show']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/turn', [\App\Http\Controllers\TurnControllerApi::class, 'index']);
+    Route::get('/iam', function (Request $request)
+    {
+        return $request->user();
+    });
+    Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+});
+
+
+
+
